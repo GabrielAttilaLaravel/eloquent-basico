0731ac6e1755617e25f5cddf996a751258d06775
“GabrielMoreno” <gabrieljmorenot@gmail.com>
2017-04-10 15:24:56 -0400

    CRUD basico.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,27 +11,44 @@
 |
 */
 use EloquentORM\User;
+use Faker\Factory as Faker;
+
 Route::get('/create', function () {
+    $faker = Faker::create();
+
     $user = User::create([
-        'name' => 'GabrielAlexander',
-        'email' => 'GabrielAlexander@a.com',
+        'name' => $faker->name,
+        'email' => $faker->email,
         'password' => bcrypt('123123'),
-        'gender' => 'm',
-        'biography' => 'Programador web'
+        'gender' => $faker->randomElement(['f', 'm']),
+        'biography' => $faker->text(255)
     ]);
 
-    return 'Usuario guardado...';
+    return $user;
 });
 
-Route::get('/update-user', function () {
+Route::get('/read/{id}', function ($id) {
+    $user = User::find($id);
 
+    return $user;
+});
 
-    $user = User::find(7);
+Route::get('/update/{id}', function ($id) {
+    $faker = Faker::create();
 
-    $user->gender = 'm';
-    $user->biography = 'Programador web';
+    $user = User::find($id);
+    $user->name = $faker->name;
+    $user->gender = $faker->randomElement(['f', 'm']);
+    $user->biography = $faker->text(255);
 
     $user->save();
 
-    return 'Usuario actualizado...';
+    return $user;
+});
+
+Route::get('/delete/{id}', function ($id) {
+    $user = User::find($id);
+    $user->delete();
+
+    return 'Usuario eliminado';
 });
